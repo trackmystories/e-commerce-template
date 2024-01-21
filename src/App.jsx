b925b728dba028fbc0959a355bd3be1818d65478
trackmystories <ashirazee@gmail.com>
2024-01-21 18:25:03 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 import LandingPage from "./Screens/LandingPage";
 import ProductPage from "./Screens/ProductPage";
+
+const NavigateButtons = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <button onClick={() => navigate("/")}>Go to Landing Page</button>
+      <button onClick={() => navigate("/products")}>Go to Product Page</button>
+    </div>
+  );
+};
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/landingpage" element={<LandingPage />} />
-        <Route path="/products" element={<ProductPage />} />
-      </Routes>
+      <div className="app-container">
+        <NavigateButtons />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/products" element={<ProductPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
